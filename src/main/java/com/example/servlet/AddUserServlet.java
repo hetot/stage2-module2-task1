bffc9ccbaf09fd9062422100b2f399fe08717d5e
hetot <shaxzod.murtazayev@mail.ru>
2022-12-18 08:03:27 +0300

    try3

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -15,14 +15,18 @@ public class AddUserServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String firstName = req.getParameter("firstName");
-        String lastName = req.getParameter("lastName");
-        if (firstName != null && lastName != null) {
-            User user = new User(firstName, lastName);
-            Warehouse.getInstance().addUser(user);
-            req.setAttribute("user", user);
+        try {
+            String firstName = req.getParameter("firstName");
+            String lastName = req.getParameter("lastName");
+            if (firstName != null && lastName != null) {
+                User user = new User(firstName, lastName);
+                Warehouse.getInstance().addUser(user);
+                req.setAttribute("user", user);
+            }
+        } catch (Exception ignored) {}
+        finally {
+            doGet(req, resp);
         }
-        doGet(req, resp);
     }
 
     @Override
